e04d9f47b4ef8e34ccb45d5aaae8a97370d583ca
Pratibhav <pra.pra.arp@gmail.com>
2023-05-20 21:51:58 +0530

    a

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Pratibhav Gupta</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
